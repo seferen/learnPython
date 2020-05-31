660408f7d66669d171e1f203f7b1f64ce0eec140
ivan <79854463009@yandex.ru>
2020-05-31 11:37:42 +0300

    Добавлен пример работы с массивами и листами

---
+++ list.py
@@ -0,0 +1,10 @@
+quarter = ['January', 'February', 'March']
+coords = [[1, 2, 3], [4, 5, 6]]
+
+if __name__ == '__main__':
+    print('First month: ', quarter[0])
+    print('Second month: ', quarter[1])
+    print('Third: ', quarter[2])
+    print('Top left: ', coords[0][0])
+    print('right bottom: ', coords[1][2])
+    print('Second month first letter: ', quarter[1][0])
